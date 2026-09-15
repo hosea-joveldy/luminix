a2016e40aa5d34d37f9382a8ecd19cda0544b503
hosea-joveldy <hoseajovel@gmail.com>
2026-09-15 22:40:56 +0700

    rice: tweak kitty theme

--- home.nix
+++ home.nix
@@ -118,7 +118,7 @@ in
   programs.kitty = {
     enable = true;
     settings = {
-      background_opacity = "0.55";
+      background_opacity = "0.65";
       background_blur = 20;
       confirm_os_window_close = 0;
       window_padding_width = 10;
